f095e81b9e283da3c51efc44563c2b86854ae553
Storozhenko <zyklon_b@open.by>
2013-02-08 16:21:51 +0300

    update

--- test_proj/SimpleLogic/src/main/java/com/sam/quest/dao/MultiDAO.java
+++
@@ -1,9 +0,0 @@
-package com.sam.quest.dao;
-
-
-public interface MultiDAO <E> {
-    public boolean insertRecord(E obj);
-    public boolean deleteRecord(E obj);
-    public E findRecord(long id, E obj);
-    public boolean updateRecord(E obj);
-}
