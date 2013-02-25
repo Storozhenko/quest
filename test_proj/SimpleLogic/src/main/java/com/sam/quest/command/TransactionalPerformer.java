26521678ae18f63f41a283ca78e51315d62425e5
Storozhenko <zyklon_b@open.by>
2013-02-25 14:14:46 +0300

    update

--- test_proj/SimpleLogic/src/main/java/com/sam/quest/command/TransactionalPerformer.java
+++ test_proj/SimpleLogic/src/main/java/com/sam/quest/command/TransactionalPerformer.java
@@ -1,5 +1,6 @@
 package com.sam.quest.command;
 
+import com.sam.quest.dao.factory.HiberDAOFactory;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +11,8 @@ public class TransactionalPerformer <E> {
 
     @Transactional
     public E executeCommand(Command <E> command) throws Exception {
-        E obj = command.execute(sessionFactory.getCurrentSession());
+        E obj = command.execute(HiberDAOFactory.getSessionFactory().openSession());
+        HiberDAOFactory.getSessionFactory().close();
         return obj;
     }
 
